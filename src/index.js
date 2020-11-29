4bdf3d9e78cf65437b46ad1e6816057d45554358
guofeng029 <guofeng029@ke.com>
2020-11-29 22:51:26 +0800

    new

---
+++ src/index.js
@@ -0,0 +1,64 @@
+
+import Hammer from 'hammerjs'
+import io from 'socket.io-client';
+
+
+const canvas = document.getElementById('draw-board')
+
+
+var ctx=canvas.getContext("2d");
+
+
+
+
+var mc = new Hammer.Manager(canvas);
+
+mc.add( new Hammer.Pan({}) );
+mc.add( new Hammer.Tap({}) );
+
+
+mc.on("panstart", startDraw);
+mc.on("panmove", draw);
+mc.on("panend", draw);
+
+let pos = {x:0,y:0}
+let enableDraw = false
+
+window.onload = function(){
+	canvas.width = window.innerWidth
+	canvas.height = window.innerHeight
+}
+
+function draw(event){
+	const e = event.srcEvent
+	if(enableDraw){
+		ctx.save();
+
+		ctx.moveTo( pos.x, pos.y );	
+		ctx.lineTo(e.offsetX, e.offsetY)
+
+  		ctx.strokeStyle = '#663300';
+        ctx.lineWidth = 10;
+
+		ctx.stroke();
+		ctx.restore();
+		pos = {x:e.offsetX, y:e.offsetY}
+	}
+}
+
+function startDraw(event){
+	const e = event.srcEvent
+	enableDraw = true
+	pos = {x:e.offsetX, y:e.offsetY}
+}
+
+function endDraw(event){
+	const e = event.srcEvent
+	enableDraw = false
+}
+
+
+var socket = io();
+
+
+socket.emit('chat message','123');
